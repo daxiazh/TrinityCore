6da5f0c607e4ebaf6cf70930810ad0b6a1f22d97
zhanghua <daxiazh@qq.com>
2017-07-01 13:50:29 +0800

    添加了一个从WoW目录建立链接的批处理

---
+++ contrib/copy_maps_windows.bat
@@ -0,0 +1,4 @@
+mklink /J vmaps "C:\World of Warcraft 3.3.5 DVD\vmaps"
+mklink /J maps "C:\World of Warcraft 3.3.5 DVD\maps"
+mklink /J dbc "C:\World of Warcraft 3.3.5 DVD\dbc"
+mklink /J cameras "C:\World of Warcraft 3.3.5 DVD\cameras"
